e9ea0a6becb2055ff995e75c677e3b8c1fe6f101
CheungLeo <102926503+CheungLeo@users.noreply.github.com>
2024-03-20 00:13:04 -0400

    Add category filter to ProductDisplay

--- src/components/productDisplay/ProductDisplay.tsx
+++ src/components/productDisplay/ProductDisplay.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 //import CreatePost from '../createPost/CreatePost';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ProductCard from '../productCard/ProductCard';
-import { DocumentData, collection, query, orderBy, onSnapshot} from "firebase/firestore"; 
+import { DocumentData, collection, query, orderBy, onSnapshot, where} from "firebase/firestore"; 
 import { db } from '../../pages/firebase.js';
 import { useEffect } from 'react';
 import React from 'react';
 import { AppContext } from '../../AppContext';
+import { Container } from 'react-bootstrap';
 
 
 
@@ -13,11 +16,17 @@ function ProductDisplay() {
   const [items, setItems] = useState<DocumentData[]>([]);
   /* eslint-disable-next-line @typescript-eslint/no-unused-vars*/ //disable linting for setSearchString never used it wont be used here
   const { searchString, setSearchString } = React.useContext(AppContext); //shared state, for search bar to communicate with product display
+  const [ queryCategory, setQueryCategory ] = useState<'WTS' | 'WTB' | 'Service' | 'Any'>('Any');
   
   // Fetch the posts from the database and update the state
   const getAllPosts = () => {
     const postsRef = collection(db, "Posts");
-    const q = query(postsRef, orderBy("id", "asc")); //Get all posts by recency (id is the timestamp of the post)
+    let q = query(postsRef, orderBy("id", "asc")); //Get all posts by recency (id is the timestamp of the post)
+    if (queryCategory !== 'Any') { //If a category is selected, filter by category
+      q = query(postsRef, orderBy("id", "asc"), where("category", "==", queryCategory));
+    }
+
+    
     const getallPosts = onSnapshot(q, (snapshot) => {
       let result: DocumentData[] = snapshot.docs.map((doc) => doc.data());
       if (searchString !== '') {
@@ -35,11 +44,21 @@ function ProductDisplay() {
     const action = getAllPosts(); //Update post list
     
     return () => action();
-  }, [searchString]);
+  }, [searchString, queryCategory]);
   
   
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Container>
+        <form>
+        <ButtonGroup>
+                <Button variant={queryCategory === 'Any' ? 'primary' : 'secondary'} onClick={() => setQueryCategory('Any')}>Any</Button>
+                <Button variant={queryCategory === 'WTS' ? 'primary' : 'secondary'} onClick={() => setQueryCategory('WTS')}>WTS</Button>
+                <Button variant={queryCategory === 'WTB' ? 'primary' : 'secondary'} onClick={() => setQueryCategory('WTB')}>WTB</Button>
+                <Button variant={queryCategory === 'Service' ? 'primary' : 'secondary'} onClick={() => setQueryCategory('Service')}>Service</Button>
+              </ButtonGroup>
+        </form>
+      </Container>
     {[...items].reverse().map((item, index) => (
         // Render a ProductCard for each item, passing the item as a prop
         <ProductCard key={index} item={item} />
