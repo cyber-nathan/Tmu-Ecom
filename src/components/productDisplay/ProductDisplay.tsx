91db3b82a46176e4e7b53ab5811ac2b1b57bf780
CheungLeo <102926503+CheungLeo@users.noreply.github.com>
2024-03-20 19:13:59 -0400

    Add sorting to ProductDisplay

--- src/components/productDisplay/ProductDisplay.tsx
+++ src/components/productDisplay/ProductDisplay.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 //import CreatePost from '../createPost/CreatePost';
+import { Navbar } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ProductCard from '../productCard/ProductCard';
@@ -8,7 +9,6 @@ import { db } from '../../pages/firebase.js';
 import { useEffect } from 'react';
 import React from 'react';
 import { AppContext } from '../../AppContext';
-import { Container } from 'react-bootstrap';
 import { Dropdown } from 'react-bootstrap';
 
 
@@ -18,22 +18,7 @@ function ProductDisplay() {
     /* eslint-disable-next-line @typescript-eslint/no-unused-vars*/ //disable linting for setSearchString never used it wont be used here
   const { searchString, setSearchString } = React.useContext(AppContext); //shared state, for search bar to communicate with product display
   const [ queryCategory, setQueryCategory ] = useState<'Items for Sale' | 'Items Wanted' | 'Academic Services' | 'Any'>('Any');
-  
-  // Fetch the posts from the database and update the state
-  const getAllPosts = () => {
-    const postsRef = collection(db, "Posts");
-    const q = query(postsRef, orderBy("id", "asc")); //Get all posts by recency (id is the timestamp of the post)
-    
-    const getallPosts = onSnapshot(q, (snapshot) => {
-      const res = snapshot.docs.map((doc) => doc.data());
-      setMasterItems(res);
-      setItems(res);
-    }, (error) => {
-      console.error("Error listening to posts:", error);
-    });
-    
-    return getallPosts;
-  };
+  const [querySort, setQuerySort] = useState<'Recency' | 'Price: Low to High' | 'Price: High to Low' | 'Alphabetical'>('Recency');
 
   useEffect(() => {
     const action = getAllPosts(); //Update post list
@@ -49,37 +34,69 @@ function ProductDisplay() {
   }, [searchString, masterItems]);
 
   useEffect(() => {
+    setQuerySort('Recency');
     if (queryCategory !== 'Any') {
       setItems(masterItems.filter((item) => item.category === queryCategory)); //filter the posts by the search string
     } else {
       setItems(masterItems);
     }
   }, [queryCategory, masterItems]);
+
+    // Fetch the posts from the database and update the state
+    const getAllPosts = () => {
+      console.log("Fetching posts, stop app if this is called in a loop there is a bug and it will overload the quota");
+      const postsRef = collection(db, "Posts");
+      const q = query(postsRef, orderBy("id", "asc")); //Get all posts by recency (id is the timestamp of the post)
+      
+      const getallPosts = onSnapshot(q, (snapshot) => {
+        const res = snapshot.docs.map((doc) => doc.data());
+        setMasterItems(res);
+        setItems(res);
+      }, (error) => {
+        console.error("Error listening to posts:", error);
+      });
+      
+      return getallPosts;
+    };
   
+    const SortPosts = (sortType: string) => {
+      if (sortType === 'Recency') {
+        setItems([...items].sort((a, b) => a.id - b.id));
+      } else if (sortType === 'Price: Low to High') {
+        setItems([...items].sort((a, b) => b.price - a.price));
+      } else if (sortType === 'Price: High to Low') {
+        setItems([...items].sort((a, b) => a.price - b.price));
+      } else if (sortType === 'Alphabetical') {
+        setItems([...items].sort((a, b) => b.prodName.localeCompare(a.prodName)));
+      }
+    };
+  
+  //Temporary style for navbar, might need change for mobile
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Container>
-        <form>
+      <Navbar style={{ width: '1000px' }} className="bg-light justify-content-between"> 
+
           <ButtonGroup>
                   <Button variant={queryCategory === 'Any' ? 'primary' : 'secondary'} onClick={() => setQueryCategory('Any')}>Any</Button>
                   <Button variant={queryCategory === 'Items for Sale' ? 'primary' : 'secondary'} onClick={() => setQueryCategory('Items for Sale')}>Items for Sale</Button>
                   <Button variant={queryCategory === 'Items Wanted' ? 'primary' : 'secondary'} onClick={() => setQueryCategory('Items Wanted')}>Items Wanted</Button>
                   <Button variant={queryCategory === 'Academic Services' ? 'primary' : 'secondary'} onClick={() => setQueryCategory('Academic Services')}>Academic Services</Button>
           </ButtonGroup>
+          
           <Dropdown>
-
           <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Sort by
+            Sort by {querySort}
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item onClick={() => setItems([...items].sort((a, b) => a.prodName.localeCompare(b.prodName)))}>Name</Dropdown.Item>
-            <Dropdown.Item onClick={() => setItems([...items].sort((a, b) => a.price - b.price))}>Price</Dropdown.Item>
-            <Dropdown.Item onClick={() => setItems([...items].sort((a, b) => a.id - b.id))}>Recency</Dropdown.Item>
+            <Dropdown.Item onClick={() => {setQuerySort('Recency'); SortPosts('Recency');}}>Recency</Dropdown.Item>
+            <Dropdown.Item onClick={() => {setQuerySort('Price: Low to High'); SortPosts('Price: Low to High');}}>Price: Low to High</Dropdown.Item>
+            <Dropdown.Item onClick={() => {setQuerySort('Price: High to Low'); SortPosts('Price: High to Low');}}>Price: High to Low</Dropdown.Item>
+            <Dropdown.Item onClick={() => {setQuerySort('Alphabetical'); SortPosts('Alphabetical');}}>Alphabetical</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
-        </form>
-      </Container>
+        
+      </Navbar>
     {[...items].reverse().map((item, index) => (
         // Render a ProductCard for each item, passing the item as a prop
         <ProductCard key={index} item={item} />
